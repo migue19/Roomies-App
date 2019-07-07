fca1413848998707c6acd3e77fdd8403f6a2943c
MacbookPro <miguelmexicano@macbook-pro-de-miguel.local>
2019-07-07 16:23:59 -0500

    Initial Commit

---
+++ Roomies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Roomies
+//
+//  Created by Miguel Mexicano on 07/07/19.
+//  Copyright © 2019 Miguel Mexicano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
